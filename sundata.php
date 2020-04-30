15dd99179ea638c731d0eea970f2660a99ef2057
bradymcgannon <61217206+bradymcgannon@users.noreply.github.com>
2020-04-29 18:08:09 -0700

    Implemented BootStrap to format

--- sundata.php
+++ sundata.php
@@ -25,98 +25,102 @@ add the buttons and stuff for autofill
 <!DOCTYPE html>
 <html>
     <head>
-        <!-- add header -->
+        <!-- Bootstrap CSS -->
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     </head>
 
-<body>
-<form method="post" action="sunresults.php">
-      <h1> Enter a location (address, city, state, etc.)</h1>
+    <body>
+        <div class="container pt-5">
+            <form method="post" action="sunresults.php">
+                <h1> Enter a location (address, city, state, etc.)</h1>
 
-        <p>Location<br/>
-           <input type="text" name="location"/>
-       </p>
+                    <p>Location<br/>
+                    <input type="text" name="location"/>
+                </p>
 
-        <!--
-        *These options should autofill the field for location, reload page with data entered
-        <p>Some Locations Around the World<br/>
-          <select name="predetermined">
-                <option value="0">Choose a city</option>
-                <option>City1</option>
-                <option>City2</option>
-                ......(more cities)
-          </select>
-        </p>
-        -->
+                    <!--
+                    *These options should autofill the field for location, reload page with data entered
+                    <p>Some Locations Around the World<br/>
+                    <select name="predetermined">
+                            <option value="0">Choose a city</option>
+                            <option>City1</option>
+                            <option>City2</option>
+                            ......(more cities)
+                    </select>
+                    </p>
+                    -->
 
-        <!--
-        *Will default to current day if none entered
-        *Create button for current date- will work the same way as the API default
-            -will just submit without entering any date
-        *Handle exception for invalid date (such as wrongly entered year)
-        -->
-       <h1>Date (Optional)</h1>
+                    <!--
+                    *Will default to current day if none entered
+                    *Create button for current date- will work the same way as the API default
+                        -will just submit without entering any date
+                    *Handle exception for invalid date (such as wrongly entered year)
+                    -->
+                <h1>Date (Optional)</h1>
 
-       <p>Month<br/>
-       <select name="month">
-                <option value="00">Choose a month</option>
-                <option value="01">January</option>
-                <option value="02">February</option>
-                <option value="03">March</option>
-                <option value="04">April</option>
-                <option value="05">May</option>
-                <option value="06">June</option>
-                <option value="07">July</option>
-                <option value="08">August</option>
-                <option value="09">September</option>
-                <option value="10">October</option>
-                <option value="11">November</option>
-                <option value="12">December</option>
-         </select>
-       </p> 
+                <p>Month<br/>
+                <select name="month">
+                            <option value="00">Choose a month</option>
+                            <option value="01">January</option>
+                            <option value="02">February</option>
+                            <option value="03">March</option>
+                            <option value="04">April</option>
+                            <option value="05">May</option>
+                            <option value="06">June</option>
+                            <option value="07">July</option>
+                            <option value="08">August</option>
+                            <option value="09">September</option>
+                            <option value="10">October</option>
+                            <option value="11">November</option>
+                            <option value="12">December</option>
+                    </select>
+                </p>
 
-       <p>Day<br/>
-       <select name="day">
-                <option value="00">Choose a day</option>
-                <option value="01">1</option>
-                <option value="02">2</option>
-                <option value="03">3</option>
-                <option value="04">4</option>
-                <option value="05">5</option>
-                <option value="06">6</option>
-                <option value="07">7</option>
-                <option value="08">8</option>
-                <option value="09">9</option>
-                <option value="10">10</option>
-                <option value="11">11</option>
-                <option value="12">12</option>
-                <option value="13">13</option>
-                <option value="14">14</option>
-                <option value="15">15</option>
-                <option value="16">16</option>
-                <option value="17">17</option>
-                <option value="18">18</option>
-                <option value="19">19</option>
-                <option value="20">20</option>
-                <option value="21">21</option>
-                <option value="22">22</option>
-                <option value="23">23</option>
-                <option value="24">24</option>
-                <option value="25">25</option>
-                <option value="26">26</option>
-                <option value="27">27</option>
-                <option value="28">28</option>
-                <option value="29">29</option>
-                <option value="30">30</option>
-                <option value="31">31</option>
-            </select>
-        </p> 
+                <p>Day<br/>
+                <select name="day">
+                            <option value="00">Choose a day</option>
+                            <option value="01">1</option>
+                            <option value="02">2</option>
+                            <option value="03">3</option>
+                            <option value="04">4</option>
+                            <option value="05">5</option>
+                            <option value="06">6</option>
+                            <option value="07">7</option>
+                            <option value="08">8</option>
+                            <option value="09">9</option>
+                            <option value="10">10</option>
+                            <option value="11">11</option>
+                            <option value="12">12</option>
+                            <option value="13">13</option>
+                            <option value="14">14</option>
+                            <option value="15">15</option>
+                            <option value="16">16</option>
+                            <option value="17">17</option>
+                            <option value="18">18</option>
+                            <option value="19">19</option>
+                            <option value="20">20</option>
+                            <option value="21">21</option>
+                            <option value="22">22</option>
+                            <option value="23">23</option>
+                            <option value="24">24</option>
+                            <option value="25">25</option>
+                            <option value="26">26</option>
+                            <option value="27">27</option>
+                            <option value="28">28</option>
+                            <option value="29">29</option>
+                            <option value="30">30</option>
+                            <option value="31">31</option>
+                        </select>
+                    </p>
 
-       <p>Year<br/>
-           <input type="number" name="year">
-       </p>
+                <p>Year<br/>
+                    <input type="number" name="year">
+                </p>
 
-        <input type="submit"> 
-        <input type="reset" value="Clear Form">     
-</form>
-</body>
-</html> 
+                    <input type="submit">
+                    <input type="reset" value="Clear Form">
+            </form>
+        </div>
+    </body>
+</html>
--- sunresults.php
+++ sunresults.php
@@ -1,127 +1,166 @@
 <?php
-//add back button
-//add formatting (ie. you entered...)
-//handle no location errors
+    //add back button
+    //add formatting (ie. you entered...)
+    //handle no location errors
 
-$linkLocation = "https://us1.locationiq.com/v1/search.php?key=dec61f8a36c608&q=".$_POST["location"]."&format=json";
-echo $linkLocation;
+    $linkLocation = "https://us1.locationiq.com/v1/search.php?key=dec61f8a36c608&q=" . $_POST["location"] . "&format=json";
+    // echo $linkLocation;
 ?>
 
 <html>
-    <div id="lat"></div>
-    <div id="lon"></div>
-    <script src="javascript/easternTime.js"></script>
-    <script>
-        var getGeocode = function(){
-            var request = new XMLHttpRequest();
-            request.open("GET", "<?php echo $linkLocation?>", true);
-            request.onload = function(){
-                var dataArray = JSON.parse(this.response);
-                for(var i = 0; i < dataArray.length; i++){
-                    var data = dataArray[i];
-                    var latitude = data.lat;
-                    var longitude = data.lon;
-                    document.getElementById("lat").innerHTML = latitude;
-                    document.getElementById("lon").innerHTML = longitude;
-                }
-                getResults();
+    <head>
+        <!-- Bootstrap CSS -->
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+        <script src="javascript/easternTime.js"></script>
+        <script>
+            var getGeocode = function(){
+                var request = new XMLHttpRequest();
+                request.open("GET", "<?php echo $linkLocation ?>", true);
+                request.onload = function(){
+                    var dataArray = JSON.parse(this.response);
+                    for(var i = 0; i < dataArray.length; i++){
+                        var data = dataArray[i];
+                        var latitude = data.lat;
+                        var longitude = data.lon;
+                        document.getElementById("lat").innerHTML = latitude;
+                        document.getElementById("lon").innerHTML = longitude;
+                    }
+                    getResults();
+                };
+                request.send();
             };
-            request.send();
-        };
 
-        var getResults = function(){
-            var request = new XMLHttpRequest();
-            request.open("GET","https://api.sunrise-sunset.org/json?lat=" + document.getElementById("lat").innerHTML + "&lng=" + document.getElementById("lon").innerHTML + "&date=" + "<?php echo $_POST["year"]?>" + "-" + "<?php echo $_POST["month"]?>" + "-" + "<?php echo $_POST["day"]?>",true);    
-            //"https://api.sunrise-sunset.org/json?lat=36.7201600&lng=-4.4203400&date=2020-04-28"
-            request.onload=function(){
-                var results= JSON.parse(this.response);
-                var data=results.results
-                console.log(data);
-                var sunrise=data.sunrise;
-                document.getElementById("sunrise").innerHTML=sunrise;
-                document.getElementById("sunriseEastern").innerHTML= utcToEastern(sunrise);
-                var sunset=data.sunset;
-                document.getElementById("sunset").innerHTML=sunset;
-                document.getElementById("sunsetEastern").innerHTML= utcToEastern(sunset);
-                var solar_noon=data.solar_noon;
-                document.getElementById("solar_noon").innerHTML=solar_noon;
-                document.getElementById("solar_noonEastern").innerHTML= utcToEastern(solar_noon);
-                var day_length=data.day_length;
-                document.getElementById("day_length").innerHTML=day_length;
-                var civil_twilight_begin=data.civil_twilight_begin;
-                document.getElementById("civil_twilight_begin").innerHTML=civil_twilight_begin;
-                document.getElementById("civil_twilight_beginEastern").innerHTML= utcToEastern(civil_twilight_begin);
-                var civil_twilight_end=data.civil_twilight_end;
-                document.getElementById("civil_twilight_end").innerHTML=civil_twilight_end;
-                document.getElementById("civil_twilight_endEastern").innerHTML= utcToEastern(civil_twilight_end);
-                var nautical_twilight_begin=data.nautical_twilight_begin;
-                document.getElementById("nautical_twilight_begin").innerHTML=nautical_twilight_begin;
-                document.getElementById("nautical_twilight_beginEastern").innerHTML= utcToEastern(nautical_twilight_begin);
-                var nautical_twilight_end=data.nautical_twilight_end;
-                document.getElementById("nautical_twilight_end").innerHTML=nautical_twilight_end;
-                document.getElementById("nautical_twilight_endEastern").innerHTML= utcToEastern(nautical_twilight_end);
-                var astronomical_twilight_begin=data.astronomical_twilight_begin;
-                document.getElementById("astronomical_twilight_begin").innerHTML=astronomical_twilight_begin;
-                document.getElementById("astronomical_twilight_beginEastern").innerHTML= utcToEastern(astronomical_twilight_begin);
-                var astronomical_twilight_end=data.astronomical_twilight_end;
-                document.getElementById("astronomical_twilight_end").innerHTML=astronomical_twilight_end;
-                document.getElementById("astronomical_twilight_endEastern").innerHTML= utcToEastern(astronomical_twilight_end);
-               
+            var getResults = function(){
+                var request = new XMLHttpRequest();
+                request.open("GET","https://api.sunrise-sunset.org/json?lat=" + document.getElementById("lat").innerHTML + "&lng=" + document.getElementById("lon").innerHTML + "&date=" + "<?php echo $_POST["year"] ?>" + "-" + "<?php echo $_POST["month"] ?>" + "-" + "<?php echo $_POST["day"] ?>",true);
+                //"https://api.sunrise-sunset.org/json?lat=36.7201600&lng=-4.4203400&date=2020-04-28"
+                request.onload=function(){
+                    var results= JSON.parse(this.response);
+                    var data=results.results
+                    console.log(data);
+                    var sunrise=data.sunrise;
+                    document.getElementById("sunrise").innerHTML=sunrise;
+                    document.getElementById("sunriseEastern").innerHTML= utcToEastern(sunrise);
+                    var sunset=data.sunset;
+                    document.getElementById("sunset").innerHTML=sunset;
+                    document.getElementById("sunsetEastern").innerHTML= utcToEastern(sunset);
+                    var solar_noon=data.solar_noon;
+                    document.getElementById("solar_noon").innerHTML=solar_noon;
+                    document.getElementById("solar_noonEastern").innerHTML= utcToEastern(solar_noon);
+                    var day_length=data.day_length;
+                    document.getElementById("day_length").innerHTML=day_length;
+                    var civil_twilight_begin=data.civil_twilight_begin;
+                    document.getElementById("civil_twilight_begin").innerHTML=civil_twilight_begin;
+                    document.getElementById("civil_twilight_beginEastern").innerHTML= utcToEastern(civil_twilight_begin);
+                    var civil_twilight_end=data.civil_twilight_end;
+                    document.getElementById("civil_twilight_end").innerHTML=civil_twilight_end;
+                    document.getElementById("civil_twilight_endEastern").innerHTML= utcToEastern(civil_twilight_end);
+                    var nautical_twilight_begin=data.nautical_twilight_begin;
+                    document.getElementById("nautical_twilight_begin").innerHTML=nautical_twilight_begin;
+                    document.getElementById("nautical_twilight_beginEastern").innerHTML= utcToEastern(nautical_twilight_begin);
+                    var nautical_twilight_end=data.nautical_twilight_end;
+                    document.getElementById("nautical_twilight_end").innerHTML=nautical_twilight_end;
+                    document.getElementById("nautical_twilight_endEastern").innerHTML= utcToEastern(nautical_twilight_end);
+                    var astronomical_twilight_begin=data.astronomical_twilight_begin;
+                    document.getElementById("astronomical_twilight_begin").innerHTML=astronomical_twilight_begin;
+                    document.getElementById("astronomical_twilight_beginEastern").innerHTML= utcToEastern(astronomical_twilight_begin);
+                    var astronomical_twilight_end=data.astronomical_twilight_end;
+                    document.getElementById("astronomical_twilight_end").innerHTML=astronomical_twilight_end;
+                    document.getElementById("astronomical_twilight_endEastern").innerHTML= utcToEastern(astronomical_twilight_end);
+
+                };
+                request.send();
             };
-            request.send(); 
-        };
-    </script>
+        </script>
     </head>
 
     <body>
-    <script>getGeocode();</script>
-    <h1>Results</h1>
-    <h2>Sunrise: </h2>
-    <h4> Universal Time:</h4>
-    <h3 id="sunrise"></h3>
-    <h4> Eastern Daylight Time:</h4>
-    <h3 id="sunriseEastern"></h3>
-    <h2>Sunset: </h2>
-    <h4> Universal Time:</h4>
-    <h3 id="sunset"></h3>
-    <h4> Eastern Daylight Time:</h4>
-    <h3 id="sunsetEastern"></h3>
-    <h2>Day Length: </h2>
-    <h3 id="day_length"></h3>
-    <h2>Solar Noon: </h2>
-    <h4> Universal Time:</h4>
-    <h3 id="solar_noon"></h3>
-    <h4> Eastern Daylight Time:</h4>
-    <h3 id="solar_noonEastern"></h3>
-    <h2>Civil Twilight Begin: </h2>
-    <h4> Universal Time:</h4>
-    <h3 id="civil_twilight_begin"></h3>
-    <h4> Eastern Daylight Time:</h4>
-    <h3 id="civil_twilight_beginEastern"></h3>
-    <h2>Civil Twilight End: </h2>
-    <h4> Universal Time:</h4>
-    <h3 id="civil_twilight_end"></h3>
-    <h4> Eastern Daylight Time:</h4>
-    <h3 id="civil_twilight_endEastern"></h3>
-    <h2>Nautical Twilight Begin: </h2>
-    <h4> Universal Time:</h4>
-    <h3 id="nautical_twilight_begin"></h3>
-    <h4> Eastern Daylight Time:</h4>
-    <h3 id="nautical_twilight_beginEastern"></h3>
-    <h2>Nautical Twilight End: </h2>
-    <h4> Universal Time:</h4>
-    <h3 id="nautical_twilight_end"></h3>
-    <h4> Eastern Daylight Time:</h4>
-    <h3 id="nautical_twilight_endEastern"></h3>
-    <h2>Astronomical Twilight Begin: </h2>
-    <h4> Universal Time:</h4>
-    <h3 id="astronomical_twilight_begin"></h3>
-    <h4> Eastern Daylight Time:</h4>
-    <h3 id="astronomical_twilight_beginEastern"></h3>
-    <h2>Astronomical Twilight End: </h2>
-    <h4> Universal Time:</h4>
-    <h3 id="astronomical_twilight_end"></h3>
-    <h4> Eastern Daylight Time:</h4>
-    <h3 id="astronomical_twilight_endEastern"></h3>
+        <div class="container pt-5">
+            <!-- getGeocode data -->
+            <script>getGeocode();</script>
+
+            <h1>Results</h1>
+        <table class="table">
+            <thead>
+              <tr>
+                <th scope="col"></th>
+                <th scope="col">Universal Time</th>
+                <th scope="col">Eastern Daylight Time</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <th scope="row">Sunrise</th>
+                <td id="sunrise"></td>
+                <td id="sunriseEastern"></td>
+              </tr>
+
+              <tr>
+                <th scope="row">Sunset</th>
+                <td id="sunset"></td>
+                <td id="sunsetEastern"></td>
+              </tr>
+
+              <tr>
+                <th scope="row">Solar Noon</th>
+                <td id="solar_noon"></td>
+                <td id="solar_noonEastern"></td>
+              </tr>
+
+              <tr>
+                <th scope="row">Civil Twilight Begin</th>
+                <td id="civil_twilight_begin"></td>
+                <td id="civil_twilight_beginEastern"></td>
+              </tr>
+
+              <tr>
+                <th scope="row">Civil Twilight End</th>
+                <td id="civil_twilight_end"></td>
+                <td id="civil_twilight_endEastern"></td>
+              </tr>
+
+              <tr>
+                <th scope="row">Nautical Twilight Begin</th>
+                <td id="nautical_twilight_begin"></td>
+                <td id="nautical_twilight_beginEastern"></td>
+              </tr>
+
+              <tr>
+                <th scope="row">Nautical Twilight End</th>
+                <td id="nautical_twilight_end"></td>
+                <td id="nautical_twilight_endEastern"></td>
+              </tr>
+
+              <tr>
+                <th scope="row">Astronomical Twilight Begin</th>
+                <td id="astronomical_twilight_begin"></td>
+                <td id="astronomical_twilight_beginEastern"></td>
+              </tr>
+
+              <tr>
+                <th scope="row">Astronomical Twilight End</th>
+                <td id="astronomical_twilight_end"></td>
+                <td id="astronomical_twilight_endEastern"></td>
+              </tr>
+
+              <tr>
+                <th scope="row">Day Length</th>
+                <td id="day_length"></td>
+              </tr>
+
+              <tr>
+                <th scope="row">Latitude</th>
+                <td id="lat"></td>
+              </tr>
+
+              <tr>
+                <th scope="row">Longitude</th>
+                <td id="lon"></td>
+              </tr>
+
+            </tbody>
+          </table>
+        </div>
     </body>
 <html>
